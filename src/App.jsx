f309423ba5a5c382ce45dd84f78af662d2840f32
SubinSundar <subinsundar9624@gmail.com>
2024-03-27 11:37:19 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,13 @@ import { MOCKEVENTS } from './Calender/conts'
 function App() {
 
   const [events, setEvents] = useState(MOCKEVENTS)
-  const [showPortal, setShowPortal] = useState(false);
-  const [portalData, setPortalData] = useState({});
+
 
   const addEvent = (date, color) => {
     const text = window.prompt("text");
     setEvents((prev) => [...prev, {date, title: text, color}])
   }
-
-  const handleOnClickEvent = (event) =>{
-    setShowPortal(true);
-    setPortalData(event);
-  }
-
-  const handlePotalClose = () => setShowPortal(false);
-
-  const handleDelete = () => {
-
-    setEvents((prevEvents) => 
-      prevEvents.filter((ev) => ev.title !== portalData.title)
-    );
-    handlePotalClose();
-  }
- 
+  
   return (
     <div>
     <Calender 
